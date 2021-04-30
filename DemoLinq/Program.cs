a1c3260dff2f2b6c01e9b6722fd0409fdb57b80b
JohnBrandt00 <notjohnbrandt@gmail.com>
2021-04-30 11:13:25 -0400

    changes

--- DemoLinq/Program.cs
+++ DemoLinq/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DemoLinq.Context;
 using DemoLinq.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DemoLinq
@@ -12,11 +13,23 @@ namespace DemoLinq
             Console.WriteLine("Hello World!");
             using(var context = new CompanyContext())
             {
-                var emp = new Employee { EmployeeId = 199, Name = "Naomi Nagata", Salary = 1000.43 };
-                context.Employees.Add(emp);
+                //var emp = new Employee { EmployeeId = 199, Name = "Naomi Nagata", Salary = 1000.43 };
+                //context.Employees.Add(emp);
+
+                Console.WriteLine(context.Employees.FirstOrDefault());
+
+                var man = new Manager { ManagerId = 100, Name = "James Holden", Employees = new List<Employee>()};
+
+                man.Employees.Add( context.Employees.FirstOrDefault());
+                context.Managers.Add(man);
                 context.SaveChanges();
 
                 context.Employees.Select(x => new { Name = x.Name }).ToList().ForEach(x => Console.WriteLine(x.Name));
+                context.Managers.Select(x => new { Name = x.Name, Emps = x.Employees }).ToList().ForEach(x =>
+                {
+                    Console.WriteLine(x.Name);
+                    x.Emps.ForEach(y => Console.Write(y.Name));
+                });
             }
         }
     }
