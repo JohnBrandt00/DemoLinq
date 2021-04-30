df53b6ca6c8bb07fbdba6f604e078f7cd73d7157
JohnBrandt00 <notjohnbrandt@gmail.com>
2021-04-30 11:48:37 -0400

    depARTMENTS

--- DemoLinq/Program.cs
+++ DemoLinq/Program.cs
@@ -19,17 +19,30 @@ namespace DemoLinq
                 Console.WriteLine(context.Employees.FirstOrDefault());
 
                 var man = new Manager { ManagerId = 100, Name = "James Holden", Employees = new List<Employee>()};
+                var dep = new Department { DepartmentId = 1, DepartmentName = "Water Department", Managers = new List<Manager>() };
 
+                dep.Managers.Add(context.Managers.FirstOrDefault());
                 man.Employees.Add( context.Employees.FirstOrDefault());
-               // context.Managers.Add(man);
-                //context.SaveChanges();
 
+                //context.Departments.Add(dep);
+                // context.Managers.Add(man);
+                //context.SaveChanges();
+               // context.SaveChanges();
                 context.Employees.Select(x => new { Name = x.Name }).ToList().ForEach(x => Console.WriteLine(x.Name));
                 context.Managers.Select(x => new { Name = x.Name, Emps = x.Employees }).ToList().ForEach(x =>
                 {
                     Console.WriteLine("Manager: {0}", x.Name);
                     x.Emps.ForEach(y => Console.Write("Employee under manager: {0}", y.Name));
                 });
+
+                var deps = (from d in context.Departments select d);
+                foreach (var item in deps)
+                {
+                    Console.WriteLine(item.DepartmentName);
+
+                    item.Managers.ForEach(x => Console.WriteLine("Managed by {0}", x.Name));
+                }
+
             }
         }
     }
